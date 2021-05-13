cf3954e3a15d5bc150bcedc3bf605a39852550eb
Apocalypsor <37431235+Apocalypsor@users.noreply.github.com>
2021-05-14 02:03:08 +0800

    Revert "Image Reverse Proxy"
    This reverts commit e38fbf6db448c4b37711cf2c104385a9603902ae.

--- rss.py
+++ rss.py
@@ -13,29 +13,29 @@ feed = FeedGenerator()
 def addEntry(entries):
     for entry in entries:
         feedEntry = feed.add_entry()
-        feedEntry.title(entry["title"])
-        feedEntry.link(href=entry["link"])
-        feedEntry.description(entry["description"])
-        feedEntry.pubDate(entry["pubDate"])
+        feedEntry.title(entry['title'])
+        feedEntry.link(href=entry['link'])
+        feedEntry.description(entry['description'])
+        feedEntry.pubDate(entry['pubDate'])
 
 
-if __name__ == "__main__":
-    os.makedirs("dist", exist_ok=True)
+if __name__ == '__main__':
+    os.makedirs('dist', exist_ok=True)
 
-    if sys.argv[1] == "dmzj":
-        feed.title("动漫之家新闻")
-        feed.description("动漫之家新闻RSS")
-        feed.link(href="http://news.dmzj.com")
-        feed.logo("https://cdn.jsdelivr.net/gh/Apocalypsor/My-Feeds/assets/dmzj.ico")
+    if sys.argv[1] == 'dmzj':
+        feed.title('动漫之家新闻')
+        feed.description('动漫之家新闻RSS')
+        feed.link(href='http://news.dmzj.com')
+        feed.logo('https://cdn.jsdelivr.net/gh/Apocalypsor/My-Feeds/assets/dmzj.ico')
         addEntry(dmzj.main())
 
-        feed.rss_file("dist/dmzj.xml")
+        feed.rss_file('dist/dmzj.xml')
 
-    if sys.argv[1] == "acg178":
-        feed.title("178动漫")
-        feed.description("178动漫RSS")
-        feed.link(href="https://acg.178.com/")
-        feed.logo("https://cdn.jsdelivr.net/gh/Apocalypsor/My-Feeds/assets/acg178.ico")
+    if sys.argv[1] == 'acg178':
+        feed.title('178动漫')
+        feed.description('178动漫RSS')
+        feed.link(href='https://acg.178.com/')
+        feed.logo('https://cdn.jsdelivr.net/gh/Apocalypsor/My-Feeds/assets/acg178.ico')
         addEntry(acg178.main())
 
-        feed.rss_file("dist/acg178.xml")
+        feed.rss_file('dist/acg178.xml')
